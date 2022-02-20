a531457b51de10244e05169b3047ee2cb01831fe
RakeshNaik2000 <rakeshnaikrathod@gmail.com>
2022-02-21 00:15:46 +0530

    new file is added

---
+++ Sort.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Sort{
+	public static void main(String args[]){
+		int a[]={12,-1,8,7,21,19};
+		Arrays.sort(a);
+		for(int item:a){
+			System.out.print(item+" ");
+		}
+	}
+}
